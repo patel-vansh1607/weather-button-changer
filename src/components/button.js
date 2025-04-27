3402a8e5efcf125ab71ae42a90a167a15a5a5889
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-27 14:25:48 +0300

    adding all the buttons

--- src/components/button.js
+++ src/components/button.js
@@ -1,13 +1,23 @@
-import { useState } from "react"
+import { useState } from "react";
+import sunny from "../images/sunny.jpg";
+import rainy from "../images/rainy.jpg";
+import cloudy from "../images/cloudy.jpg";
 
 const ButtonChanger = () => {
-    const [background, setBackground] = useState("")
-    
-    return(
-        <div>
+  const [background, setBackground] = useState('url("/src/images/sunny.jpg")');
 
-        </div>
-    )
-}
+  const changeBackground = (imageUrl) => {
+    setBackground(`url("${imageUrl}")`);
+  };
 
-export default ButtonChanger
+  return (
+    <div >
+      <h1>Select Weather</h1>
+      <button onClick={() => changeBackground(sunny)}>Sunny</button>
+      <button onClick={() => changeBackground(rainy)}>Rainy</button>
+      <button onClick={() => changeBackground(cloudy)}>Cloudy</button>
+    </div>
+  );
+};
+
+export default ButtonChanger;
