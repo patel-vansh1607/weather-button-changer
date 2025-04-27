38921554f04d160d1aef00e4d3d19c6ac1483140
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-27 14:27:23 +0300

    inbound styling

--- src/components/button.js
+++ src/components/button.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import sunny from "../images/sunny.jpg";
 import rainy from "../images/rainy.jpg";
 import cloudy from "../images/cloudy.jpg";
+import thunderStorm from "../images/thunderstorm.jpeg"
 
 const ButtonChanger = () => {
   const [background, setBackground] = useState('url("/src/images/sunny.jpg")');
@@ -11,11 +12,24 @@ const ButtonChanger = () => {
   };
 
   return (
-    <div >
+    <div 
+    style={{
+        backgroundImage: background,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '20px'
+      }}
+        >
       <h1>Select Weather</h1>
       <button onClick={() => changeBackground(sunny)}>Sunny</button>
       <button onClick={() => changeBackground(rainy)}>Rainy</button>
       <button onClick={() => changeBackground(cloudy)}>Cloudy</button>
+      <button onClick={() => changeBackground(thunderStorm)}>ThunderStorm</button>
     </div>
   );
 };
